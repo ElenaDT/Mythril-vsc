fc20779f576ef08e5c23bf533d38d5f767fb0c56
ElenaDT <elenadi.to@gmail.com>
2024-05-25 19:37:56 +0200

    estensione stampa report in md in un file

--- extension.js
+++ extension.js
@@ -18,8 +18,8 @@ function activate(context) {
 
         const fileName = vscode.window.activeTextEditor.document.fileName;
         const baseName = vscode.workspace.asRelativePath(fileName);
-        const terminal = vscode.window.createTerminal('Myth Analyze');
-        terminal.sendText(`myth analyze "./${baseName}"`);
+        const terminal = vscode.window.createTerminal('Myth: Analyze');
+        terminal.sendText(`myth analyze ./${baseName} -o markdown > ${baseName}.md`);
         terminal.show();
     });
 
