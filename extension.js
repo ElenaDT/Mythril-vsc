f4bf238a39e9b3463747dd90c74e8e3fd6a488e8
ElenaDT <elenadi.to@gmail.com>
2024-07-27 13:30:11 +0200

    refactoring con ESlint e config ESlint

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 const vscode = require('vscode');
-const utils = require('./utils.js');
 const path = require('path');
+const utils = require('./utils.js');
 
 function activate(context) {
   const disposable = vscode.commands.registerCommand('mythril-vsc.analyze', analyzeCommand);
@@ -9,54 +9,29 @@ function activate(context) {
 
 async function analyzeCommand(fileUri) {
   try {
-    const filePath = fileUri ? fileUri.fsPath : getActiveTextEditorFilePath();
+    const filePath = fileUri ? fileUri.fsPath : utils.getActiveEditorFilePath();
 
     if (!utils.isSolidityFile(filePath)) {
       throw new Error('This command is only available for Solidity files (.sol).');
     }
 
-    getAnalisysContext(filePath);
-  } catch (error) {
-    vscode.window.showErrorMessage(error.message);
+    analyzeFile(filePath);
+  } catch (e) {
+    vscode.window.showErrorMessage(e.message);
   }
 }
 
 function deactivate() {}
 
-async function getAnalisysContext(filePath) {
-    const fileDirectory = path.dirname(filePath);
-    const baseName = utils.getBaseName(filePath);
 
-    await analyzeFile(fileDirectory, baseName);
-}
+async function analyzeFile(filePath) {
+  const mythVscConfig = vscode.workspace.getConfiguration('mythril-vsc');
+  const execTimeout = mythVscConfig.get('executionTimeout', 60);
+  const execMode = mythVscConfig.get('executionMode', 'docker');
+  const fileDir = path.dirname(filePath);
+  const baseName = utils.getBaseName(filePath);
 
-async function analyzeFile(fileDirectory, baseName) {
-  const mythrilVscConfig = vscode.workspace.getConfiguration('mythril-vsc');
-  const executionTimeout = mythrilVscConfig.get('executionTimeout', 60);
-  const executionMode = mythrilVscConfig.get('executionMode', 'docker');
-
-
-//TODO creare funzione di utility per decidere che comando lanciare
-//[IMPLEMENT] apertura automatica dell'output.md
-
-  const terminal = vscode.window.createTerminal({ name:'Myth: Analyze File', message: `*** Mythril: starting analysis for ${baseName}... ***` });
-  let command;
-
-  if (executionMode === 'docker') {
-    command = `docker run --rm -v ${fileDirectory}:/tmp mythril/myth analyze /tmp/${baseName} -o markdown --execution-timeout ${executionTimeout} > ./${baseName}-output.md`;
-  } else {
-    command = `myth analyze ./${baseName} -o markdown --execution-timeout ${executionTimeout} > ./${baseName}-output.md`;
-  }
-  
-  terminal.show();
-  terminal.sendText(command);
-  }
-
-//[IMPLEMENT] keybinding
-//TODO deve diventare funzione di utility
-function getActiveTextEditorFilePath() {
-  const editor = vscode.window.activeTextEditor;
-  return editor ? editor.document.fileName : undefined;
+utils.launchCommand(baseName, fileDir, execTimeout, execMode);
 }
 
 module.exports = {
--- utils.js
+++ utils.js
@@ -10,7 +10,31 @@ function isSolidityFile(filePath) {
     return path.extname(filePath).toLowerCase() === '.sol';
 }
 
+function launchCommand(baseName, fileDir, execTimeout, execMode){
+//[IMPLEMENT] apertura automatica dell'output.md
+
+  const terminal = vscode.window.createTerminal({ name:'Myth: Analyze File', message: `*** Mythril: starting analysis for ${baseName}... ***` });
+  let command;
+
+  if (execMode === 'docker') {
+    command = `docker run --rm -v ${fileDir}:/tmp mythril/myth analyze /tmp/${baseName} -o markdown --execution-timeout ${execTimeout} > ./${baseName}-output.md`;
+  } else {
+    command = `myth analyze ./${baseName} -o markdown --execution-timeout ${execTimeout} > ./${baseName}-output.md`;
+  }
+  
+  terminal.show();
+    terminal.sendText(command);
+}
+
+//[IMPLEMENT] keybinding
+function getActiveEditorFilePath() {
+  const editor = vscode.window.activeTextEditor;
+  return editor ? editor.document.fileName : undefined;
+}
+
 module.exports = {
     getBaseName,
-    isSolidityFile
+    isSolidityFile,
+    launchCommand,
+    getActiveEditorFilePath,
 };
