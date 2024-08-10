90f1970eb2a0414c6bd861d19cb54da261c47ab2
ElenaDT <elenadi.to@gmail.com>
2024-08-10 17:06:15 +0200

    fix manuale merge uscito male

--- utils.js
+++ utils.js
@@ -1,7 +1,5 @@
 const vscode = require('vscode');
 const path = require('path');
-const { spawn } = require('child_process');
-const fs = require('fs');
 
 function getFileContext(filePath) {
   const mythVscConfig = vscode.workspace.getConfiguration('mythril-vsc'); 
@@ -24,40 +22,19 @@ function isSolidityFile(filePath) {
 }
 
 function getCommand(baseName, fileDir, execTimeout, execMode){
-  const outputPath = `./${baseName}-output.md`;  
-  let command = `-o markdown --execution-timeout ${execTimeout} > ${outputPath}`;
+  let command = `-o markdown --execution-timeout ${execTimeout}`;
 
   if (execMode === 'docker') {
     command = `docker run --rm -v ${fileDir}:/tmp mythril/myth analyze /tmp/${baseName} ${command}`;
   } else {
     command = `myth analyze ./${baseName} ${command}`;
   }
-  return { command, outputPath };
+  return command;
 }
-
-function launchCommand(baseName, fileDir, command, outputPath) {
-  const fullFilePath= `${fileDir}/${baseName}-output.md`;
-  const terminal = vscode.window.createTerminal({
-    name:  `myth analyze: ${baseName}`, 
-    message: `*** Mythril: starting analysis for ${baseName}... ***`
-  });
-
-  terminal.sendText(command);
-  terminal.show();
-  terminal.sendText(`exit`);
-
-  vscode.window.onDidCloseTerminal((terminal) => {
-    if (terminal.name === `myth analyze: ${baseName}` && terminal.exitStatus.code === 0) {
-      vscode.window.showInformationMessage(`Myth: output saved in ${outputPath}.`);
-      vscode.commands.executeCommand('vscode.open', vscode.Uri.file(fullFilePath));
-    } 
-  });
-};
 
 module.exports = {
   getFileContext,
   isSolidityFile,
-  launchCommand,
   getActiveEditorFilePath,
   getCommand
 };
