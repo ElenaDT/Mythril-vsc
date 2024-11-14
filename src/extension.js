ff214b9620fcf5b68b0ab370490fffc5ec96a702
ElenaDT <elenadi.to@gmail.com>
2024-11-14 23:01:50 +0100

    formattazione e errore per docker spento

--- src/extension.js
+++ src/extension.js
@@ -1,21 +1,26 @@
-/* eslint-disable strict */
+'use strict';
+
 const path = require('path');
 const fs = require('fs');
 const Docker = require('dockerode');
 const vscode = require('vscode');
-
 const docker = new Docker();
 
 function normalizePath(filePath) {
   if (process.platform === 'win32') {
-    return filePath.replace(/\\/g, '/').replace(/^([A-Za-z]):/, '/host_mnt/$1').toLowerCase();
+    return filePath
+      .replace(/\\/g, '/')
+      .replace(/^([A-Za-z]):/, '/host_mnt/$1')
+      .toLowerCase();
   }
   return filePath;
 }
 
 function getCompilerVersion(filePath) {
   const fileContent = fs.readFileSync(filePath, 'utf8');
-  const pragmaLine = fileContent.split('\n').find(line => line.startsWith('pragma solidity'));
+  const pragmaLine = fileContent
+    .split('\n')
+    .find((line) => line.startsWith('pragma solidity'));
   if (pragmaLine) {
     const versionRange = pragmaLine.split(' ')[2];
     const cleanVersion = versionRange.replace(/[^0-9.]/g, '').trim();
@@ -27,18 +32,35 @@ function getCompilerVersion(filePath) {
 }
 
 async function ensureDockerImage(imageName) {
+  try {
+    // Controlla se Docker è in esecuzione
+    await docker.ping();
+  } catch (err) {
+    vscode.window.showErrorMessage(
+      'Error: Docker Desktop is not running. Please start Docker Desktop.'
+    );
+    console.error('Error:', err);
+    return;
+  }
+
   const images = await docker.listImages();
-  const imageExists = images.some(image => image.RepoTags && image.RepoTags.includes(imageName));
-  console.log('*** imageExists ',imageExists);
+  const imageExists = images.some(
+    (image) => image.RepoTags && image.RepoTags.includes(imageName)
+  );
+  console.log('*** imageExists ', imageExists);
   if (!imageExists) {
     vscode.window.showInformationMessage(`Pulling Docker image: ${imageName}`);
     await new Promise((resolve, reject) => {
       docker.pull(imageName, {}, (err, stream) => {
         if (err) {
-          vscode.window.showErrorMessage(`Failed to pull Docker image: ${err.message}`);
+          vscode.window.showErrorMessage(
+            `Failed to pull Docker image: ${err.message}`
+          );
           return reject(err);
         }
-        docker.modem.followProgress(stream, (err, res) => err ? reject(err) : resolve(res));
+        docker.modem.followProgress(stream, (err, res) =>
+          err ? reject(err) : resolve(res)
+        );
       });
     });
   }
@@ -58,17 +80,27 @@ async function launchCommand(baseName, fileDir) {
   }
 
   const dockerSourceFilePath = normalizePath(sourceFilePath);
-  console.log('Mounting file:', { originalSourceFile: sourceFilePath, dockerSourceFile: dockerSourceFilePath });
+  console.log('Mounting file:', {
+    originalSourceFile: sourceFilePath,
+    dockerSourceFile: dockerSourceFilePath,
+  });
 
   const imageName = 'mythril/myth:latest';
   await ensureDockerImage(imageName);
 
   const containerOptions = {
     Image: imageName,
-    Cmd: ['sh', '-c', `myth analyze /tmp/${baseName} --solv ${solcVersion} -o markdown --execution-timeout 60`],
+    Cmd: [
+      'sh',
+      '-c',
+      `myth analyze /tmp/${baseName} --solv ${solcVersion} -o markdown --execution-timeout 60`,
+    ],
     Tty: false,
-    HostConfig: { AutoRemove: true, Binds: [`${dockerSourceFilePath}:/tmp/${baseName}`] },
-    WorkingDir: '/tmp'
+    HostConfig: {
+      AutoRemove: true,
+      Binds: [`${dockerSourceFilePath}:/tmp/${baseName}`],
+    },
+    WorkingDir: '/tmp',
   };
 
   await vscode.window.withProgress(
@@ -77,64 +109,87 @@ async function launchCommand(baseName, fileDir) {
       title: 'Analyzing contract...',
       cancellable: true,
     },
-    (progress, token) => new Promise((resolve, reject) => {
-      docker.createContainer(containerOptions)
-        .then(container => {
-          let output = '';
-          let hasError = false;
+    (progress, token) =>
+      new Promise((resolve, reject) => {
+        docker
+          .createContainer(containerOptions)
+          .then((container) => {
+            let output = '';
+            let hasError = false;
 
-          container.attach({ stream: true, stdout: true, stderr: true }, (err, stream) => {
-            if (err) {return rejectWithError('Attach error:', err, reject);}
-
-            stream.on('data', data => {
-              const chunk = data.toString();
-              console.log('Container output:', chunk);
-              output += chunk;
-
-              if (chunk.includes('Solc experienced a fatal error') || 
-                  chunk.includes('SyntaxError') || 
-                  chunk.includes('mythril.interfaces.cli [ERROR]: Traceback') ||
-                  chunk.includes('ValueError: Invalid version string')) {
-                hasError = true;
-              }
-            });
-
-            stream.on('end', () => {
-              if (!hasError) {
-                try {
-                  const formattedOutput = formatOutput(output);
-                  if (formattedOutput.trim()) {
-                    fs.writeFileSync(fullPath, formattedOutput);
-                    vscode.commands.executeCommand('vscode.open', vscode.Uri.file(fullPath));
-                  }
-                  resolve();
-                } catch (err) {
-                  rejectWithError('Error writing file:', err, reject);
+            container.attach(
+              { stream: true, stdout: true, stderr: true },
+              (err, stream) => {
+                if (err) {
+                  return rejectWithError('Attach error:', err, reject);
                 }
-              } else {
-                resolve();
+
+                stream.on('data', (data) => {
+                  const chunk = data.toString();
+                  console.log('Container output:', chunk);
+                  output += chunk;
+
+                  if (
+                    chunk.includes('Solc experienced a fatal error') ||
+                    chunk.includes('SyntaxError') ||
+                    chunk.includes(
+                      'mythril.interfaces.cli [ERROR]: Traceback'
+                    ) ||
+                    chunk.includes('ValueError: Invalid version string')
+                  ) {
+                    hasError = true;
+                  }
+                });
+
+                stream.on('end', () => {
+                  if (!hasError) {
+                    try {
+                      const formattedOutput = formatOutput(output);
+                      if (formattedOutput.trim()) {
+                        fs.writeFileSync(fullPath, formattedOutput);
+                        vscode.commands.executeCommand(
+                          'vscode.open',
+                          vscode.Uri.file(fullPath)
+                        );
+                      }
+                      resolve();
+                    } catch (err) {
+                      rejectWithError('Error writing file:', err, reject);
+                    }
+                  } else {
+                    resolve();
+                  }
+                });
+
+                stream.on('error', (err) => {
+                  hasError = true;
+                  rejectWithError('Stream error:', err, reject);
+                });
               }
+            );
+
+            container
+              .start()
+              .catch((err) =>
+                rejectWithError('Container start error:', err, reject)
+              );
+
+            token.onCancellationRequested(() => {
+              container
+                .stop()
+                .then(() => {
+                  vscode.window.showInformationMessage(
+                    'Myth-VSC: analysis cancelled.'
+                  );
+                  reject(new Error('Myth-VSC: analysis cancelled.'));
+                })
+                .catch((err) => rejectWithError('Stop error:', err, reject));
             });
-
-            stream.on('error', err => {
-              hasError = true;
-              rejectWithError('Stream error:', err, reject);
-            });
-          });
-
-          container.start().catch(err => rejectWithError('Container start error:', err, reject));
-
-          token.onCancellationRequested(() => {
-            container.stop()
-              .then(() => {
-                vscode.window.showInformationMessage('Myth-VSC: analysis cancelled.');
-                reject(new Error('Myth-VSC: analysis cancelled.'));
-              })
-              .catch(err => rejectWithError('Stop error:', err, reject));
-          });
-        })
-        .catch(err => rejectWithError('Container creation error:', err, reject));
-    })
+          })
+          .catch((err) =>
+            rejectWithError('Container creation error:', err, reject)
+          );
+      })
   );
 }
 
@@ -155,19 +210,25 @@ function rejectWithError(message, err, reject) {
 
 function analyzeCommand(fileUri) {
   try {
-    const filePath = fileUri ? fileUri.fsPath : vscode.window.activeTextEditor.document.fileName;
-    if (!filePath) {throw new Error('No file selected');}
+    const filePath = fileUri
+      ? fileUri.fsPath
+      : vscode.window.activeTextEditor.document.fileName;
+    if (!filePath) {
+      throw new Error('No file selected');
+    }
 
     const baseName = path.basename(filePath);
     const fileDir = path.dirname(filePath);
 
     if (filePath.endsWith('.sol')) {
-      launchCommand(baseName, fileDir).catch(err => {
+      launchCommand(baseName, fileDir).catch((err) => {
         console.error('Launch command error:', err);
         vscode.window.showErrorMessage(`Myth-VSC: ${err.message}`);
       });
     } else {
-      throw new Error('This command is only available for Solidity files (.sol).');
+      throw new Error(
+        'This command is only available for Solidity files (.sol).'
+      );
     }
   } catch (err) {
     console.error('Analyze command error:', err);
@@ -177,8 +238,10 @@ function analyzeCommand(fileUri) {
 
 module.exports = {
   activate: (context) => {
-    const disposable = vscode.commands.registerCommand('mythril-vsc.analyze', analyzeCommand);
+    const disposable = vscode.commands.registerCommand(
+      'mythril-vsc.analyze',
+      analyzeCommand
+    );
     context.subscriptions.push(disposable);
-  }
+  },
 };
-
--- src/utils.js
+++ src/utils.js
@@ -2,54 +2,66 @@
 
 const vscode = require('vscode');
 const path = require('path');
-const fs = require('fs');
+//const fs = require('fs');
 
-function getFileContext(filePath) {
-  const mythVscConfig = vscode.workspace.getConfiguration('mythril-vsc'); 
-  const baseName = path.basename(filePath);
-  const fileDir = path.dirname(filePath);
-  const projectDir = path.dirname(fileDir);
-  const execTimeoutON = mythVscConfig.get('executionTimeoutEnabled', false);
-  const execTimeout = mythVscConfig.get('executionTimeout', 90);
-
-  return { baseName, fileDir, projectDir, execTimeoutON, execTimeout };
-}
-
-function getActiveEditorFilePath() {
-  const editor = vscode.window.activeTextEditor;
-  return editor ? editor.document.fileName : undefined;
-}
-
-function isSolidityFile(filePath) {
-  return path.extname(filePath).toLowerCase() === '.sol';
-}
-
-function getCommand(baseName, fileDir, projectDir, execTimeoutON, execTimeout, solcVer) {
-  const timeoutArg = execTimeoutON ? ` --execution-timeout ${execTimeout}` : '';
-  const mythArgs = `--solv ${solcVer} -o markdown${timeoutArg}`;
-  const command = `analyze /tmp/contracts/${baseName} ${mythArgs}`;
-
-  return `${command}`;
-}
-
-function getCompilerVersion(filePath) {
-  const fileContent = fs.readFileSync(filePath, 'utf8');
-  const pragmaLine = fileContent.split('\n').find(line => line.startsWith('pragma solidity'));
-
-  if (pragmaLine) {
-    const versionRange = pragmaLine.split(' ')[2];
-    const cleanVersion = versionRange.replace(/;/g, '').replace(/\r/g, '').trim();
-    return cleanVersion;
-  } else {
-    vscode.window.showErrorMessage('Myth-VSC: Solc version not found');
-    return null;
+function getFileContext (filePath) {
+  return {
+    mythVscConfig : vscode.workspace.getConfiguration('mythril-vsc');
+    baseName : path.basename(filePath);
+    fileDir : path.dirname(filePath);
+    projectDir : path.dirname(fileDir);
+    execTimeoutON : mythVscConfig.get('executionTimeoutEnabled', false);
+    execTimeout : mythVscConfig.get('executionTimeout', 90);
   }
 }
 
+// function getActiveEditorFilePath() {
+//   const editor = vscode.window.activeTextEditor;
+//   return editor ? editor.document.fileName : undefined;
+// }
+
+// function isSolidityFile(filePath) {
+//   return path.extname(filePath).toLowerCase() === '.sol';
+// }
+
+// function getCommand(
+//   baseName,
+//   fileDir,
+//   projectDir,
+//   execTimeoutON,
+//   execTimeout,
+//   solcVer
+// ) {
+//   const timeoutArg = execTimeoutON ? ` --execution-timeout ${execTimeout}` : '';
+//   const mythArgs = `--solv ${solcVer} -o markdown${timeoutArg}`;
+//   const command = `analyze /tmp/contracts/${baseName} ${mythArgs}`;
+
+//   return `${command}`;
+// }
+
+// function getCompilerVersion(filePath) {
+//   const fileContent = fs.readFileSync(filePath, 'utf8');
+//   const pragmaLine = fileContent
+//     .split('\n')
+//     .find((line) => line.startsWith('pragma solidity'));
+
+//   if (pragmaLine) {
+//     const versionRange = pragmaLine.split(' ')[2];
+//     const cleanVersion = versionRange
+//       .replace(/;/g, '')
+//       .replace(/\r/g, '')
+//       .trim();
+//     return cleanVersion;
+//   } else {
+//     vscode.window.showErrorMessage('Myth-VSC: Solc version not found');
+//     return null;
+//   }
+// }
+
 module.exports = {
   getFileContext,
-  isSolidityFile,
-  getActiveEditorFilePath,
-  getCommand,
-  getCompilerVersion
+  // isSolidityFile,
+  // getActiveEditorFilePath,
+  // getCommand,
+  // getCompilerVersion,
 };
