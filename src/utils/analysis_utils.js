f6a524c5b47c55efbf5082d7d55243779f60a590
ElenaDT <elenadi.to@gmail.com>
2025-01-26 01:26:54 +0100

    refactoring

--- src/utils/analysis_utils.js
+++ src/utils/analysis_utils.js
@@ -5,7 +5,93 @@ const Docker = require('dockerode');
 const docker = new Docker();
 const { PassThrough } = require('stream');
 
-async function runDockerAnalysis(
+const handleCancellation = async (container) => {
+  if (container) {
+    vscode.window.showInformationMessage(
+      "Annullamento dell'analisi in corso. Attendere la conferma."
+    );
+    try {
+      await container.stop();
+      vscode.window.showInformationMessage('Analisi annullata correttamente.');
+    } catch (err) {
+      console.error("Errore durante l'annullamento del container:", err);
+    }
+  }
+};
+
+const processToFollow = async (
+  progress,
+  token,
+  containerOptions,
+  outputUri
+) => {
+  let container;
+
+  token.onCancellationRequested(() => handleCancellation(container));
+
+  try {
+    progress.report({ message: 'Creazione del container' });
+    container = await docker.createContainer(containerOptions);
+
+    progress.report({ message: "Avvio dell'analisi..." });
+
+    const stream = await container.attach({
+      stream: true,
+      stdout: true,
+      stderr: true,
+    });
+
+    const { stdOut, stdErr } = await demultiplexStream(stream);
+
+    let output = '';
+    let errorOutput = '';
+
+    stdOut.on('data', (chunk) => {
+      output += chunk;
+      progress.report({ message: "Elaborazione dell'analisi..." });
+    });
+
+    stdErr.on('data', (chunk) => {
+      vscode.window.showErrorMessage(`Analisi fallita: ${chunk.trim()}`);
+      return;
+    });
+
+    await container.start();
+    await container.wait();
+    if (token.isCancellationRequested) return;
+
+    progress.report({ message: 'Salvataggio dei risultati.' });
+
+    await vscode.workspace.fs.writeFile(outputUri, Buffer.from(output, 'utf8'));
+
+    const document = await vscode.workspace.openTextDocument(outputUri);
+    await vscode.window.showTextDocument(document, {
+      preview: false,
+    });
+
+    vscode.window.showInformationMessage('Analisi completata con successo.');
+  } catch (err) {
+    if (err.message !== 'canceled' && !token.isCancellationRequested) {
+      vscode.window.showErrorMessage(
+        `Errore durante l'analisi: ${err.message}`
+      );
+      console.error("Errore durante l'analisi:", err);
+    }
+  }
+};
+
+const demultiplexStream = async (stream) => {
+  const stdOut = new PassThrough();
+  const stdErr = new PassThrough();
+
+  docker.modem.demuxStream(stream, stdOut, stdErr);
+  stdOut.setEncoding('utf8');
+  stdErr.setEncoding('utf8');
+
+  return { stdOut, stdErr };
+};
+
+const runDockerAnalysis = async (
   fileUri,
   fileName,
   outputUri,
@@ -14,7 +100,7 @@ async function runDockerAnalysis(
   config,
   workspaceFolder,
   nodeModulesUri
-) {
+) => {
   const executionTimeout = config.get('executionTimeout', 60);
   const containerOptions = {
     Image: 'mythril/myth:latest',
@@ -35,96 +121,16 @@ async function runDockerAnalysis(
     WorkingDir: '/tmp',
   };
 
-  const handleCancellation = async (container) => {
-    if (container) {
-      vscode.window.showInformationMessage(
-        "Annullamento dell'analisi in corso. Attendere la conferma."
-      );
-      try {
-        await container.stop();
-        vscode.window.showInformationMessage(
-          'Analisi annullata correttamente.'
-        );
-      } catch (err) {
-        console.error("Errore durante l'annullamento del container:", err);
-      }
-    }
-  };
-
-  const processToFollow = async (progress, token) => {
-    let container;
-
-    try {
-      progress.report({ message: 'Creazione del container' });
-      container = await docker.createContainer(containerOptions);
-
-      progress.report({ message: "Avvio dell'analisi..." });
-      const stream = await container.attach({
-        stream: true,
-        stdout: true,
-        stderr: true,
-      });
-
-      const stdOut = new PassThrough();
-      const stdErr = new PassThrough();
-
-      docker.modem.demuxStream(stream, stdOut, stdErr);
-      stdOut.setEncoding('utf8');
-      stdErr.setEncoding('utf8');
-
-      let output = '';
-      let errorOutput = '';
-
-      stdOut.on('data', (chunk) => {
-        output += chunk;
-        progress.report({ message: "Elaborazione dell'analisi..." });
-      });
-
-      stdErr.on('data', (chunk) => {
-        vscode.window.showErrorMessage(`Analisi fallita: ${chunk.trim()}`);
-        return;
-      });
-
-      await container.start();
-
-      token.onCancellationRequested(() => handleCancellation(container));
-
-      await container.wait();
-
-      if (token.isCancellationRequested) return;
-
-      progress.report({ message: 'Salvataggio dei risultati.' });
-
-      await vscode.workspace.fs.writeFile(
-        outputUri,
-        Buffer.from(output, 'utf8')
-      );
-
-      const document = await vscode.workspace.openTextDocument(outputUri);
-      await vscode.window.showTextDocument(document, {
-        preview: false,
-      });
-
-      vscode.window.showInformationMessage('Analisi completata con successo.');
-    } catch (err) {
-      if (err.message !== 'canceled' && !token.isCancellationRequested) {
-        vscode.window.showErrorMessage(
-          `Errore durante l'analisi: ${err.message}`
-        );
-        console.error("Errore durante l'analisi:", err);
-      }
-    }
-  };
-
   await vscode.window.withProgress(
     {
       location: vscode.ProgressLocation.Notification,
       title: 'Analisi del contratto in corso',
       cancellable: true,
     },
-    (progress, token) => processToFollow(progress, token)
+    (progress, token) =>
+      processToFollow(progress, token, containerOptions, outputUri)
   );
-}
+};
 
 module.exports = {
   runDockerAnalysis,
